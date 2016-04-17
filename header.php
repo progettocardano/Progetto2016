0ef89e193b3286c0a46f19e70a6ab79aad8122dc
5aiadriancastro <5aiadriancastro@gmail.com>
2016-04-17 17:02:08 +0200

    Prima revisione. Aggiunto Bootstrap e piccoli accorgimenti

---
+++ header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="it">
+	<head>
+		<meta charset="utf8" />
+		<meta http-equiv="X-UA-COMPATIBLE" content="IE=edge" />
+		<meta name="viewport" content="
+			width=device-width,
+			initial-scale=1,
+			maximum-scale=1,
+			user-scalable=no
+		" />
+		
+		<title>Homepage</title>
+		
+		<link rel="stylesheet" href="template/css/bootstrap.css" />
+		<script type="text/javascript" src="template/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="template/js/jquery-2.2.3.min.js"></script>
+		<script type="text/javascript" src="template/js/script.js"></script>
+		
+	</head>
+	<body>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <!-- Toggle del menu quando in mobile -->
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle Navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <!-- TODO: aggiungere il logo -->
+                    <a class="navbar-brand" href="#">
+                        <img alt="Cardano Pavia" src="template/images/cardano.png" />
+                    </a>
+                </div>
+                
+                <!-- Link di navigazione -->
+                <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="index.php">Pagina Principale</a></li>
+                        <li><a href=""></a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
