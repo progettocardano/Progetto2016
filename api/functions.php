9a0cbb4680867217f138a1a4c6a53b5e68106b84
5aiadriancastro <5aiadriancastro@gmail.com>
2016-05-26 09:37:03 +0200

    Update functions.php

--- api/functions.php
+++ api/functions.php
@@ -1,6 +1,6 @@
 <?php
-    function die_message($message, $sucess, $data) {
-        $message = array("message" => $message, "success" => $success, $data = null);
+    function die_message($message, $success, $data) {
+        $message = array("message" => $message, "success" => $success, "data" => null);
         if($data) $message["data"] = $data;
         die(json_encode($message));
     }
