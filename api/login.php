b65ed4258a6497c9292e7e19ad7d6e0f3a0fb568
5aiadriancastro <5aiadriancastro@gmail.com>
2016-05-30 09:32:45 +0200

    Update login.php

--- api/login.php
+++ api/login.php
@@ -2,16 +2,18 @@
     require_once("../includes/connect.php");
     require_once("functions.php");
     header("Content-type: text/json");
+	$method = $_SERVER["REQUEST_METHOD"];
+	if($method != "POST") die_message("Richiesta " . $method . " non accettata", false); 
     //non sono stati ricevuti username o password
-    if(! (isset($_GET["username"]) || isset($_GET["password"]))) {
+    if(! (isset($_POST["username"]) || isset($_POST["password"]))) {
         die_message("Non è stato inserito username o password", false);
     }
-    if(!isset($_GET["userType"])) {
+    if(!isset($_POST["userType"])) {
         die_message("Non è stato inserito il tipo di utente", false);
     }
-    $username = $_GET["username"];
-    $password = crypt($_GET["password"], "$1$2$1npubhsaywn");
-    $userType = ($_GET["userType"] == "true") ? "studenti": "docenti";
+    $username = $_POST["username"];
+    $password = crypt($_POST["password"], "$1$2$1npubhsaywn");
+    $userType = ($_POST["userType"] == "true") ? "studenti": "docenti";
     $query = "SELECT * FROM {$userType} WHERE username = '{$username}' AND password = '{$password}'";
     $result =  $DB->query($query);
     if($result) {
